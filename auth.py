b9d39b80385312675f67c0f0a26aee7cfde9a526
motyayy <motyayy@gmail.com>
2026-05-09 17:30:21 +0300

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,55 @@
+from customtkinter import *
+from PIL import Image
+import sys
+import os
+
+def resource_path(relative_path):
+    """ Отримує абсолютний шлях до ресурсів, працює для dev та для PyInstaller """
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+class AuthWindow(CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("700x400")
+        self.title("Вхід")
+        self.resizable(True, False)
+        self.name = ""
+
+        # --- ліва частина --
+        self.left_frame = CTkFrame(self)
+        self.left_frame.pack(side="left", fill="both")
+
+        # ТУТ ЗМІНЮЄМО КАРТІНКУ ДЛЯ ФОНУ
+        img_ctk = CTkImage(light_image=Image.open(resource_path("img.png")), size=(450, 400))
+        self.img_label = CTkLabel(self.left_frame, text="Welcome", image=img_ctk, font=("Helvetica", 50, "bold"))
+        self.img_label.pack()
+
+        self.right_frame = CTkFrame(self)
+        self.right_frame.pack_propagate(False)
+        self.right_frame.pack(side="right", fill="both", expand="True")
+
+        CTkLabel(self.right_frame, text="LogiTalk").pack(pady=60)
+
+        self.name_entry = CTkEntry(self.right_frame, placeholder_text="Ім'я")
+        self.name_entry.pack(fill="x", padx=10, pady=10)
+
+        self.ip_entry = CTkEntry(self.right_frame, placeholder_text="IP адресса сервера")
+        self.ip_entry.pack(fill="x", padx=10, pady=10)
+
+        self.port_entry = CTkEntry(self.right_frame, placeholder_text="Порт сервера")
+        self.port_entry.pack(fill="x", padx=10, pady=10)
+
+        self.connect_button = CTkButton(self.right_frame,text="УВІЙТИ", command=self.open_messenger)
+        self.connect_button.pack(fill="x", padx=10, pady=10)
+
+    def open_messenger(self):
+        self.name = self.name_entry.get()
+        self.ip = self.ip_entry.get()
+        self.port = int(self.port_entry.get())
+        self.destroy()
+
+AuthWindow().mainloop()
